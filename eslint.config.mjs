8c493595cae7538672dbf9483c0a5abe3ab408dc
pos1234 <paulosegebeyehu5@gmail.com>
2025-01-14 16:40:39 +0300

    Disable eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,12 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "no-console": "warn", // Warn for console logs instead of errors
-      "no-unused-vars": "error", // Ensure no unused variables
-    },
-  },
 ];
 
 export default eslintConfig;
